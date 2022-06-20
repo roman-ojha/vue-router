b20b3d0461fbed2a33e8cdd1eae73b3ee31c7e51
Roman-Ojha <razzroman98@gmail.com>
2022-06-20 18:57:25 +0545

    fix: update Navigation

--- src/Topic_Wise/01_Introduction/components/Navigation.vue
+++ src/Topic_Wise/01_Introduction/components/Navigation.vue
@@ -4,7 +4,7 @@
         *) router-link:
             Note how instead of using regular a tags, we use a custom component router-link to create links. This allows Vue Router to change the URL without reloading the page, handle URL generation as well as its encoding. We will see later how to benefit from these features.
      -->
-    <router-link to="/home">Home</router-link>
+    <router-link to="/">Home</router-link>
     <router-link to="/posts">Posts</router-link>
     <router-link to="/create">CreatePost</router-link>
   </nav>
