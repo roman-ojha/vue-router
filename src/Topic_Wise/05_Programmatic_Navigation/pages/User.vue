9a287e18cb9f3548792a079cca73aac1f87a6b6e
Roman-Ojha <razzroman98@gmail.com>
2022-07-17 13:14:33 +0545

    docs: replace, traverse, history manipulation

--- src/Topic_Wise/05_Programmatic_Navigation/pages/User.vue
+++ src/Topic_Wise/05_Programmatic_Navigation/pages/User.vue
@@ -2,14 +2,32 @@
   <div>
     <h1>Hello {{ $route.params.id }}</h1>
     <!-- https://router.vuejs.org/guide/essentials/navigation.html#navigate-to-a-different-location -->
+
+    <!-- Replace current location -->
+    <!-- It acts like router.push, the only difference is that it navigates without pushing a new history entry, as its name suggests - it replaces the current entry. -->
     <button
-      @click="$router.push({ path: `/user/${$route.params.id}/profile` })"
+      @click="$router.replace({ path: `/user/${$route.params.id}/profile` })"
     >
       Profile
     </button>
-    <button @click="$router.push(`/user/${$route.params.id}/posts`)">
+    <button @click="$router.replace(`/user/${$route.params.id}/posts`)">
       Posts
     </button>
+
+    <!-- Traverse history -->
+    <!-- This method takes a single integer as parameter that indicates by how many steps to go forward or go backward in the history stack, similar to window.history.go(n). -->
+    <button @click="$router.go(-3)">Go back -3</button>
+    <button @click="$router.go(3)">Go back +3</button>
+
+    <!-- 
+      # History Manipulation:
+          You may have noticed that router.push, router.replace and router.go are counterparts of window.history.pushState, window.history.replaceState and window.history.go, and they do imitate the window.history APIs.
+
+          Therefore, if you are already familiar with Browser History APIs, manipulating history will feel familiar when using Vue Router.
+
+          It is worth mentioning that Vue Router navigation methods (push, replace, go) work consistently no matter the kind of history option is passed when creating the router instance.
+     -->
+
     <router-view></router-view>
   </div>
 </template>
